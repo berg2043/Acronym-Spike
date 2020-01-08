acbf777db31c5ad6d1978288a91c9502172c1a6b
Phillip Berg <phillipb1991@msn.com>
2020-01-08 12:28:38 -0600

    improved stress test (1,000,000 words in 5.5 sec)

--- stress_progress/stressprogress.js
+++ stress_progress/stressprogress.js
@@ -1,33 +1,20 @@
 const pool = require('./pool');
 
 
-function words (num) {
-  let arr = []
-
-  let dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-  for(let i=0; i<num; i++){
-    let word = ''
-    for(let j = 0; j<4; j++){
-      word += dict[Math.floor(Math.random()*dict.length)]
+async function words (num) {
+    const arr = []
+    let dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    for(let i=0; i<num; i++){
+      let word = ''
+      for(let j = 0; j<4; j++){
+        word += dict[Math.floor(Math.random()*dict.length)]
+      }
+      arr.push(word);  
     }
-    arr.push(word);  
-  }
-  
-  let blings = ''
-  for(let i = 0; i<arr.length; i++){
-    blings += `$${i+1}, `
-  }
-  blings = blings.slice(0,blings.length-2);
-  const queryText = `SELECT * FROM "words" WHERE "word" IN (${blings});`;
-  pool.query(queryText, arr).then(results=>{
-    console.log(num,results.rows);
-  }).catch(err=>{
-    console.log(err);
-  })
-  
+  const results = await pool.query(`EXPLAIN ANALYZE SELECT * FROM "words" WHERE "word" = ANY($1::varchar(50)[]);`, [arr]).catch(err=>{return err})
+  return results.rows[0];
 }
 
 // words(1000);
 // console.log('next');
-words(60000)
+words(1000000).then(r=>{console.log(r)})
